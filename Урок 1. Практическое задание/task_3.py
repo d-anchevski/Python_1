0df19d193334e399f8e8ea59ead069472d972a5e
d.anchevski <anchevski.analytics@gmail.com>
2022-12-14 21:34:18 +0300

    В Задачи №№1-6 Урока №1 добавлены решения

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+num_str = input("Enter a natural number: \n")
+num_str2 = num_str * 2
+num_str3 = num_str * 3
+
+result=int(num_str)+int(num_str2)+int(num_str3)
+print(f"Solution: {num_str} + {num_str2} + {num_str3} = {result}")
