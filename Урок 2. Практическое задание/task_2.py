a75bf6ae3c4baf6b0e372407aab347f46d4ae7cc
d.anchevski <anchevski.analytics@gmail.com>
2022-12-17 13:47:57 +0300

    Добавлены решения задач №№1-6 Урока №2

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,21 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+
+# N1 Creating a str-type variable
+var_str = input("Enter integer numbers, separated by spaces\n")
+
+# N2 Creating a list from the digits of the created variable
+var_lst = var_str.split(" ")
+print(f"This is the list gotten from your digits : {var_lst}")
+
+# N3 Calculating an operator to control cycle condition
+op_1 = len(var_lst) % 2
+
+# N4 Creating a bubble-type sorting (with double step and operation control)
+for i in range(0, len(var_lst) - 1 - op_1, 2):
+    var_x = var_lst[i + 1]
+    var_lst[i + 1] = var_lst[i]
+    var_lst[i] = var_x
+
+print(f"This is the moved list: {var_lst}")
