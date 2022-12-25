54f206d73a7a6e7c044e7238eb2d7ae5ee39aa68
d.anchevski <anchevski.analytics@gmail.com>
2022-12-25 21:58:47 +0300

    Добавлены решения для задач №№1-7 Практического задания №4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,16 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+# Traditional way:
+init_lst = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+proc_lst = []
+for i in range(1, len(init_lst)):
+    if init_lst[i] > init_lst[i - 1]:
+        proc_lst.append(init_lst[i])
+print(f"This is made traditionally: {proc_lst}")
+
+# LS-expression (generator expression) way:
+proc_lst = (init_lst[i] for i in range(1, len(init_lst)) if init_lst[i] > init_lst[i - 1])
+print(f"This is made by LC-expression: {list(proc_lst)}")  # Wrapping into list to see the contents of generator
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -6,3 +6,17 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+init_lst = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+"""proc_set = set()
+(proc_set.add(el) for el in init_lst)
+print(f"This is the unordered processed set {list(proc_set)}")"""
+
+proc_lst = (init_lst[i] for i in range(0, len(init_lst)) if init_lst[i] not in
+            (init_lst[j] for j in range(i + 1, len(init_lst))) and init_lst[i] not in (init_lst[k] for k in
+                                                                                       range(0, i)))
+# I believe this could have benn resolved easier - but though here I did use even three generator-expressions,
+# two of which are even in-built )))
+
+print(f"The resulting array is: {list(proc_lst)}")
