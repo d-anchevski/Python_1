130f0ed5a05a3ffb9508273d9011d83d40de4a56
d.anchevski <anchevski.analytics@gmail.com>
2023-01-15 15:59:22 +0300

    Попытка №2 подгрузить решения ДЗ№6 в ПР.

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,43 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+# Class block
+class Worker:
+
+    def __init__(self, name="Undefined", surname="Undefined", position="Unset", wage=0, bonus=0):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    pos_lst = []  # A list of all class instances
+
+    def __init__(self, name="Undefined", surname="Undefined", position="Unset", wage=0, bonus=0):
+        super().__init__(name, surname, position, wage, bonus)
+        Position.pos_lst.append(self)
+
+    def get_full_name(self):
+        return self.name + " " + self.surname
+
+    def get_total_income(self):
+        return self._income.get("wage") + self._income.get("bonus")
+
+    def __str__(self):
+        return f"The employee {self.get_full_name()} has the total income of {self.get_total_income()}"
+
+
+# Client code
+
+ps_adp = Position("Dmitri", "Anchevski", "CPI", 3000, 500)
+ps_aiv = Position("Inesse", "Anchevskaya", "I1K", 2000)
+
+
+for el in Position.pos_lst:
+    print(f"\nThe employee's N{Position.pos_lst.index(el)+1} full name is {el.get_full_name()}")
+    print(f"Total income of the employee is {el.get_total_income()}")
+    print(f"Str testing: {el}")
+
